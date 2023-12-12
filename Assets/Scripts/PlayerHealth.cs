65c611b430f593dd88764883c3e3b85de412445e
avery's personal hell <94857895+avyisonline@users.noreply.github.com>
2023-12-12 20:53:01 +0100

    Bug Fixing LOL

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int health = 100;
+    public TMP_Text healthCounter;
 
     void Start()
     {
-
+        healthCounter.text = health.ToString();
     }
 
     void Update()
@@ -26,6 +28,7 @@ public class PlayerHealth : MonoBehaviour
         if (collision.gameObject.CompareTag("NPC"))
         {
             health -= 5;
+            healthCounter.text = health.ToString();
         }
 
     }
--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO.Pipes;
 using TMPro;
 using UnityEngine;
-using TMPro;
 using UnityEngine.SceneManagement;
 using System;
 
