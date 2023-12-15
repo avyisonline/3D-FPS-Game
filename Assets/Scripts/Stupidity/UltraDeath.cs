ac992c8e33b19456874ffc8405de86a65c3a359d
avery's personal hell <94857895+avyisonline@users.noreply.github.com>
2023-12-15 23:13:45 +0100

    Last Minute Bugfixes

--- Assets/Scripts/Stupidity/UltraDeath.cs
+++ Assets/Scripts/Stupidity/UltraDeath.cs
@@ -4,11 +4,8 @@ using UnityEngine;
 
 public class UltraDeath : MonoBehaviour
 {
-    public bool crash() { return crash(); }
-
     void Start()
     {
-        crash();
+        Application.Quit();
     }
-
 }
